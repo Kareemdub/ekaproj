fbea204a0f00d8b3d95ba36b18bd78931b8280c7
Kareemdub <kareem.werlander@metropolia.fi>
2025-08-21 13:01:23 +0300

    mod02 tehtävät tehty

--- mod 2/mod 02 tehtävät.py
+++ mod 2/mod 02 tehtävät.py
@@ -1,5 +1,6 @@
 #tehtävä 1
 
+
 nimi = input("Anna nimesi:") # pyydetään käyttäjän nimi
 print (f"Terve, {nimi}!") # tervehditään käyttäjää
 
@@ -46,9 +47,44 @@ print("Lukujen keskiarvo on: "  + str(keskiarvo))
 leiviska1 = float(input("Syötä leivisköjen määrä: "))
 naula1 = float(input("Syötä naulojen määrä: "))
 luoti1 = float(input("Syötä luotien määrä: "))
-#testataan laskutapaa (clueless)
-gramma1 = float(20 * 32 * 13,3 * leiviska1)
-gramma2 = float(32 * 13,3 * naula1)
-gramma3 = float(13.3)
+#tavaroiden muunnokset grammoiksi
+gramma1 = float(20 * 32 * 13.3 * leiviska1)
+gramma2 = float(32 * 13.3 * naula1)
+gramma3 = float(13.3 * luoti1)
 
-print(f"grammat: {gramma1 + gramma2 + gramma3}")
+yhteispaino = gramma1 + gramma2 +gramma3
+#printataan vastaus kilogrammoissa ja grammoissa
+print(f"Massa nykymittojen mukaan: \n"
+      f"{yhteispaino // 1000} kilogrammaa ja"
+      f" {yhteispaino % 1000:.2f} grammaa")
+
+#tehtävä 6
+
+#haetaan random
+import random
+#tehdään randomisti generoitu lista kolmelle ja neljälle numerolle.
+kolminumeroinen_koodi = [random.randint(0, 9) for l in range(3)]
+print(f"Kolminumeroinen koodinne: {kolminumeroinen_koodi}")
+
+nelinumeroinen_koodi = [random.randint(1, 6) for o in range(4)]
+print(f"Nelinumeroinen koodinne: {nelinumeroinen_koodi}")
+
+
+# tein tämän näköisen aluksi, mutta en siitä, joten menin etsimään netistä ratkaisua
+"""import random
+
+numero1 = random.randint(0, 9)
+numero2 = random.randint(0, 9)
+numero3 = random.randint(0, 9)
+
+input("Pyydä kolminumeroinen koodi painamalla enter.")
+print(f"Kolminumeroinen koodi: {numero1, numero2, numero3}")
+
+numero4 = random.randint(1, 6)
+numero5 = random.randint(1, 6)
+numero6 = random.randint(1, 6)
+numero7 = random.randint(1, 6)
+
+input("Pyydä nelinumeroinen koodi painamalla enter.")
+
+print(f"Nelinumeroinen koodi: {numero4, numero5, numero6, numero7}")"""
